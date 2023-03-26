8b14d4df7497c296a159e68450ad6b3beb507cd2
Natasha Andreeva <natasha@miro.com>
2023-03-26 21:14:33 +0200

    Initial Commit

---
+++ Shower/ShowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowerApp.swift
+//  Shower
+//
+//  Created by Natasha Andreeva on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
